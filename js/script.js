fafd4f8a30626e3c7eb3127aa5bbb82115697f87
LucaRiccio <lucariccio888@gmail.com>
2020-07-06 17:47:57 +0200

    Esercizio completato

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var nome = prompt ("Nome?");
+var cognome = prompt ("Cognome?");
+var colore = prompt ("Il Tuo Colore Preferito?");
+var numero = 19;
+
+document.getElementById('password').innerHTML = 'La tua password è: '+ nome + cognome + colore + numero;
